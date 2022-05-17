8af16972ef5fd555495ee4af74ba9ae3f99a5424
Ethan James <lskywalker416@gmail.com>
2022-05-17 10:10:38 -0600

    ifuckinghatecoors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,15 @@
 let options = {
     headers: {
       'User-Agent': 'catgirlsaresexy.org random image fetcher',
-      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
       "Access-Control-Allow-Credentials": "true",
       "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
       "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
     }
   }
+
+
 
 
 
