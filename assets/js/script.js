f25429f8cca12e345b321ba6602b7116d23c47dd
Ethan James <lskywalker416@gmail.com>
2022-05-17 11:07:57 -0600

    eruytre

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ axios.get('https://nekos.moe/api/v1/random/image?nsfw=false')
   .then(function (response) {
       const imageID = response;
         console.log(imageID.status);
-        console.log(imageID.data.images.id);
+        console.log(imageID.data);
       //console.log(response.data.images.id);
   })
   .catch(function (error) {
