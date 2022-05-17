908dd92d568488b5109645d23c553102e0875845
Ethan James <lskywalker416@gmail.com>
2022-05-17 10:18:33 -0600

    h o l y  f u c k i n g  s h i t

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ let options = {
       'User-Agent': 'catgirlsaresexy.org random image fetcher',
       'Access-Control-Allow-Origin': 'nekos.moe',
       'Content-Type': 'application/json',
-      "Access-Control-Allow-Credentials": "true",
       "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
       "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
     }
