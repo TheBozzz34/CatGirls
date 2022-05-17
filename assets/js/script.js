eaf6c85ff757e269121b978ca88c71e601a666c9
Ethan James <lskywalker416@gmail.com>
2022-05-17 09:36:48 -0600

    add image get

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+import axios from 'axios';
+
+axios.get('http://api.nekos.fun:8080/api/foxgirl')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    console.log('done');
+  });
+
 console.log("Hello World!");
 
 document.body.style.backgroundColor = "#563d7c";
